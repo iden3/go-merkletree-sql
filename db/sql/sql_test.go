debafb6e35e0e37f78e64e32a5d5f63b1e113e37
Oleg Lomaka <oleg.lomaka@gmail.com>
2021-11-13 09:32:57 +0200

    remove old test database initialization

--- db/sql/sql_test.go
+++ db/sql/sql_test.go
@@ -2,11 +2,8 @@ package sql
 
 import (
 	"errors"
-	"fmt"
 	"io"
-	"os"
-	"os/user"
-	"strconv"
+	"sync/atomic"
 	"testing"
 
 	"github.com/iden3/go-merkletree-sql"
@@ -18,92 +15,28 @@ import (
 )
 
 var maxMTId uint64 = 0
-var cleared = false
-
 var dbPool = go_test_pg.Pgpool{
 	BaseName:   "merkletree_sql",
 	SchemaFile: "./schema.sql",
 	Skip:       false,
 }
-func setupDB2(t *testing.T) (*sqlx.DB, error) {
-	db := dbPool.WithStdEmpty(t)
-	return sqlx.NewDb(db, "pgx"), nil
-}
-
-func setupDB() (*sqlx.DB, error) {
-	var err error
-	host := os.Getenv("PGHOST")
-	if host == "" {
-		host = "localhost"
-	}
-	port, _ := strconv.Atoi(os.Getenv("PGPORT"))
-	if port == 0 {
-		port = 5432
-	}
-	dbUser := os.Getenv("PGUSER")
-	if dbUser == "" {
-		osUser, err := user.Current()
-		if err != nil {
-			return nil, err
-		}
-		dbUser = osUser.Username
-	}
-	password := os.Getenv("PGPASSWORD")
-	//if password == "" {
-	//	return nil, errors.New("No PGPASSWORD envvar specified")
-	//}
-	dbname := os.Getenv("PGDATABASE")
-	if dbname == "" {
-		dbname = "test"
-	}
-
-	psqlconn := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host,
-		port,
-		dbUser,
-		password,
-		dbname,
-	)
-	dbx, err := sqlx.Connect("postgres", psqlconn)
-	if err != nil {
-		return nil, err
-	}
-
-	// clear MerkleTree table
-	//if !cleared {
-	dbx.Exec("TRUNCATE TABLE mt_roots")
-	dbx.Exec("TRUNCATE TABLE mt_nodes")
-	cleared = true
-	//}
-
-	return dbx, nil
-}
 
 type SqlStorageBuilder struct{}
 
 func (builder *SqlStorageBuilder) NewStorage(t *testing.T) merkletree.Storage {
+	db := dbPool.WithStdEmpty(t)
+	dbx := sqlx.NewDb(db, "pgx")
 
-	dbx, err := setupDB2(t)
-	if err != nil {
-		t.Fatal(err)
-		return nil
-	}
+	mtId := atomic.AddUint64(&maxMTId, 1)
 
-	sto, err := NewSqlStorage(dbx, maxMTId)
-	if err != nil {
-		t.Fatal(err)
-		return nil
-	}
-	maxMTId++
+	sto, err := NewSqlStorage(dbx, mtId)
+	require.NoError(t, err)
 
 	return sto
 }
 
 func TestSql(t *testing.T) {
-	//sto := sqlStorage(t)
 	builder := &SqlStorageBuilder{}
-
 	test.TestAll(t, builder)
 }
 
